c8b5cc7189f2820d6aeccf2d16709c3528a510a5
Comrade-Riley <rileygamer001@gmail.com>
2020-12-10 13:44:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# python-paint
+ A paint app made with python with the libraries, pygame,pickle,PyZenity and easygui.
---
+++ main.py
@@ -0,0 +1,98 @@
+import pygame,pickle,PyZenity,easygui
+class Screen():
+	def __init__(self,width,heigth):
+		self.width = width
+		self.heigth = heigth
+		self.display = pygame.display.set_mode([self.width,self.heigth])
+
+class Dot():
+	def __init__(self,x,y,radius,color):
+		self.x = x
+		self.y = y
+		self.radius = radius
+		self.color = color
+	def draw(self):
+		self.sprite = pygame.draw.circle(screen.display,self.color,[self.x,self.y],self.radius)
+
+def checkEvents():
+	global dots
+	global i
+	global color
+	global size
+	global mode
+	global running
+	keys = pygame.key.get_pressed()
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+		deldots = []
+		if pygame.mouse.get_pressed()[0]:
+			mousex, mousey = pygame.mouse.get_pos()
+			if mode == 1:
+				dots[f'Dot{len(dots)+1}'] = Dot(mousex,mousey,size,color)
+			elif mode == 2:
+				for dot in dots:
+					click = dots[dot].sprite.collidepoint([mousex,mousey])
+					if click:
+						deldots.append(dot)
+			for dot in deldots:
+				del dots[dot]
+			deldots = []
+
+
+		if keys[pygame.K_ESCAPE]:
+			running = False
+		if event.type == pygame.MOUSEBUTTONDOWN:		
+			if event.button == 4:
+				size += 1
+			elif event.button == 5:
+				size -= 1
+			elif pygame.mouse.get_pressed()[2]:
+				if mode == 2:
+					mode = 0
+				mode += 1
+		if keys[pygame.K_LCTRL] and keys[pygame.K_x]:
+			i+=1
+			if i == len(colors)-1:
+				i = 0
+			color = colors[i]
+		if keys[pygame.K_LCTRL] and keys[pygame.K_c]:
+			dots = {}
+		if keys[pygame.K_LCTRL] and keys[pygame.K_s]:
+			file = easygui.filesavebox(filetypes='*.pyp')
+			if file != None:
+				if file[-3] + file[-2] + file[-1] != 'pyp':
+					file += '.pyp'
+				pickle.dump(dots, open(file, "wb"))
+		if keys[pygame.K_LCTRL] and keys[pygame.K_l]:
+			file = easygui.fileopenbox(filetypes=['*.pyp'])
+			if file != None:
+				if file[-3] + file[-2] + file[-1] != 'pyp':
+					Question = PyZenity.Warning("That is not compatible filetype, save files have to be '.pyp' please try again with a '.pyp' file.")
+				else:
+					dots = pickle.loads(open(file,'rb').read())
+
+
+
+
+i=0
+colors = [[0,0,0],[255,255,255],[255,0,0],[0,255,0],[0,0,255],[255,251,0],[255,105,180],[255,69,0],[128,0,128]]
+color = [0,0,0]
+size = 25
+mode = 1
+dots = {}
+def main():
+	screen.display.fill([255,255,255])
+	for dot in dots:
+		dots[dot].draw()
+	pygame.display.flip()
+	checkEvents()
+
+
+
+pygame.init()
+screen = Screen(500,500)
+running = True
+while running:
+	main()
+pygame.quit()
